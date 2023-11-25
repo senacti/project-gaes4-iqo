97abe1a7417304c59e11482629c8709d9d0d3eaa
Stephy20KarollCelis <126290714+Stephy20KarollCelis@users.noreply.github.com>
2023-11-25 16:00:54 -0500

    agrego actualizaciones Django

--- 06 - Trimestre/IQO_Project/IQO/venta/admin.py
+++ 06 - Trimestre/IQO_Project/IQO/venta/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from . models import Warranty_Type, Warranty, Brand, Product, Sale
 
-# Register your models here.
+#admin.site.register(Warranty_Type)
+#admin.site.register(Warranty)
+#admin.site.register(Brand)
+#admin.site.register(Product)
+#admin.site.register(Sale)
+
+@admin.register(Warranty_Type)
+class warranty_typeAdmin(admin.ModelAdmin):
+    list_display = ('warranty_type_name',)
+    
+@admin.register(Warranty)
+class warrantyAdmin(admin.ModelAdmin):
+    list_display = ('warranty_description', 'warranty_time', 'warranty_type',)
+    list_filter = ('warranty_type',)
+    list_per_page = 2
+    
+@admin.register(Brand)
+class brandAdmin(admin.ModelAdmin):
+    list_display = ('name_brand',)
+    list_per_page = 2
+    
+@admin.register(Product)
+class productAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_description', 'product_reference', 'product_value', 'product_quantity', 'brand',)
+    list_editable = ('product_value', 'product_quantity',)
+    search_fields = ('product_name',)
+    list_per_page = 2
+    
+@admin.register(Sale)
+class saleAdmin(admin.ModelAdmin):
+    list_display = ('sale_date', 'quantity', 'price', 'subtotal', 'vat', 'total', 'warranty', 'product',) #falta empleado y pedidoclien por que las tablas no se han creado
+    list_editable = ('price',)
+    list_per_page = 2
--- 06 - Trimestre/IQO_Project/IQO/venta/models.py
+++ 06 - Trimestre/IQO_Project/IQO/venta/models.py
@@ -1,36 +1,5 @@
 from django.db import models
 
-
-class Brand(models.Model):
-    name_brand = models.CharField(max_length=50, verbose_name="Nombre_Marca")
-    
-    def __str__(self):
-        return self.name_brand
-    
-    class Meta:
-        verbose_name = "Marca"
-        verbose_name_plural = "Marcas"
-        db_table = "marca"
-        ordering = ['id']
-        
-
-class Product(models.Model):
-    product_name = models.CharField(max_length=50, verbose_name="Nombre_Producto")
-    product_description = models.TextField(max_length=50, verbose_name="Descripcion_Producto")
-    product_reference = models.CharField(max_length=50, verbose_name="Referencia_Producto")
-    product_value = models.PositiveIntegerField(verbose_name="Valor_Producto")
-    product_quantity = models.IntegerField(verbose_name="Cantida_Producto")
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.product_name
-    
-    class Meta:
-        verbose_name = "Producto"
-        verbose_name_plural = "Productos"
-        db_table = "producto"
-        ordering = ['id']
-
 class Warranty_Type(models.Model):
     warranty_type_name = models.CharField(max_length=50, verbose_name="Nombre_Tipo_Garantia")
     
@@ -44,3 +13,70 @@ class Warranty_Type(models.Model):
         ordering = ['id']
 
 
+class Warranty(models.Model):
+    warranty_description = models.TextField(max_length=50, verbose_name="Descripcion_Garantía")
+    warranty_time = models.DateField(verbose_name="Tiempo_Garantía")
+    warranty_type = models.ForeignKey(Warranty_Type, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.warranty_description
+    
+    class Meta:
+        verbose_name = "Garantia"
+        verbose_name_plural = "Garantias"
+        db_table = "garantia"
+        ordering = ['id']
+        
+        
+class Brand(models.Model):
+    name_brand = models.CharField(max_length=50, verbose_name="Nombre_Marca")
+    
+    def __str__(self):
+        return self.name_brand
+    
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+        db_table = "marca"
+        ordering = ['id']
+        
+        
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name="Nombre_Producto")
+    product_description = models.TextField(max_length=50, verbose_name="Descripcion_Producto")
+    product_reference = models.CharField(max_length=50, verbose_name="Referencia_Producto")
+    product_value = models.PositiveIntegerField(verbose_name="Valor_Producto")
+    product_quantity = models.IntegerField(verbose_name="Cantidad_Producto")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.product_name
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        db_table = "producto"
+        ordering = ['id']
+
+
+class Sale(models.Model):
+    sale_date = models.DateTimeField(verbose_name="Fecha_Venta")
+    quantity = models.IntegerField(verbose_name="Cantidad")
+    price = models.PositiveIntegerField(verbose_name="Precio")
+    subtotal = models.PositiveIntegerField(verbose_name="Subtotal")
+    vat = models.PositiveIntegerField(verbose_name="Iva")
+    total = models.PositiveIntegerField(verbose_name="Total")
+    # falta empleado por que es una tabla de usuarios 
+    # falta pedidoclien tambien es una tabla que necesita de usuarios
+    warranty = models.ForeignKey(Warranty, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.sale_date
+    
+    class Meta:
+        verbose_name = "Venta"
+        verbose_name_plural = "Ventas"
+        db_table = "venta"
+        ordering = ['id']
+        
