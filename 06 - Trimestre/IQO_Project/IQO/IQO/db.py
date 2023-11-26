037558cb0249f6f4812b29fd921aab3e836179c7
Alison Valoyes <126203688+Alison433@users.noreply.github.com>
2023-11-26 03:47:34 -0500

    Add files via upload

--- 06 - Trimestre/IQO_Project/IQO/IQO/db.py
+++ 06 - Trimestre/IQO_Project/IQO/IQO/db.py
@@ -2,13 +2,34 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SQLITE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+
+
+
 MYSQL = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbiqo',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Trinity433*',
         'HOST': 'localhost',
-        'PORT': '3306'
+        'PORT': '3306',
+				
     }
 }
+
+#POSTGRESQL = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'db',
+#        'USER': 'postgres',
+#        'PASSWORD': 'password',
+#        'HOST': 'localhost',
+#        'PORT': '5432'
+#    }
+#}
